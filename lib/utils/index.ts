02aa23ff5c27fb15ab258bfc1fdc436612bb2663
ladytrekker <ladytrekker@protonmail.com>
2022-11-02 21:59:37 +0100

    Add more marketplace requests to lib

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -35,9 +35,13 @@ export {
   KNSContract,
 } from "./kns";
 export {
+  createMarketplaceUser,
   getMarketplaceProject,
   getMarketplaceProjects,
   getMarketplaceUser,
+  loginMarketplaceUser,
+  updateMarketplaceUser,
+  verifyMarketplaceUser,
 } from "./marketplace";
 export { prettifySeconds } from "./prettifySeconds";
 export { prettifyUrl } from "./prettifyUrl";
