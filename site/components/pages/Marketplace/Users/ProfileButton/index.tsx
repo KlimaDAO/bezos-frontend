f5766bc3f45e10a20ddf683c3f35ca29eeed2389
ladytrekker <ladytrekker@protonmail.com>
2023-01-18 13:27:06 +0100

    Fix incorrect import

--- site/components/pages/Marketplace/Users/ProfileButton/index.tsx
+++ site/components/pages/Marketplace/Users/ProfileButton/index.tsx
@@ -1,8 +1,8 @@
 import { ButtonPrimary } from "@klimadao/lib/components";
 import { t } from "@lingui/macro";
 import ModeEditOutlinedIcon from "@mui/icons-material/ModeEditOutlined";
+import { useResponsive } from "hooks/useResponsive";
 import { FC } from "react";
-import { useResponsive } from "../../hooks/useResponsive";
 import * as styles from "./styles";
 
 /** A responsive button to allow the user to edit their profile */
