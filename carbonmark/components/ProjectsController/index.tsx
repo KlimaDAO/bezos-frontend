9720318e39597cc52ebd4e5574cf49e5f261213e
Michael Collins <michaeljosefcollins@gmail.com>
2023-01-28 10:09:06 +1100

    (#18) Toggle component

--- carbonmark/components/ProjectsController/index.tsx
+++ carbonmark/components/ProjectsController/index.tsx
@@ -1,7 +1,10 @@
 import { cx } from "@emotion/css";
 import { ButtonPrimary } from "@klimadao/lib/components";
+import GridViewIcon from "@mui/icons-material/GridView";
+import ListIcon from "@mui/icons-material/List";
 import TuneIcon from "@mui/icons-material/Tune";
 import { SearchInput } from "components/SearchInput";
+import { Toggle } from "components/Toggle";
 import { FC, HTMLAttributes } from "react";
 import * as styles from "./styles";
 
@@ -19,6 +22,13 @@ export const ProjectsController: FC<ProjectControllerProps> = (props) => {
         className={styles.filterButton}
         icon={<TuneIcon />}
         label="Filters"
+      />
+      <Toggle
+        onChange={console.log}
+        options={[
+          { content: <GridViewIcon />, value: "grid" },
+          { content: <ListIcon />, value: "list" },
+        ]}
       />
     </div>
   );
