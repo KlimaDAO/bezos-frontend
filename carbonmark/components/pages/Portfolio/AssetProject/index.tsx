003dbcf903f885f8d42e73ebbbbf5b1a6ddd04cc
ladytrekker <ladytrekker@protonmail.com>
2023-01-31 16:34:19 +0100

    First render of asset project data

---
+++ carbonmark/components/pages/Portfolio/AssetProject/index.tsx
@@ -0,0 +1,23 @@
+import { Text } from "@klimadao/lib/components";
+import { Asset } from "@klimadao/lib/types/carbonmark";
+import { Trans } from "@lingui/macro";
+import { Card } from "components/Card";
+import { useRouter } from "next/router";
+import { FC } from "react";
+
+interface Props {
+  assetsData: Asset;
+}
+
+export const AssetProject: FC<Props> = (props) => {
+  const { locale } = useRouter();
+
+  return (
+    <Card key={props.assetsData.projectId}>
+      <Text t="h4">
+        <Trans id="portfolio.asset.title">{props.assetsData.projectName}</Trans>
+      </Text>
+      <Text t="caption">Quantity Available: {props.assetsData.balance}</Text>
+    </Card>
+  );
+};
--- carbonmark/components/pages/Portfolio/index.tsx
+++ carbonmark/components/pages/Portfolio/index.tsx
@@ -1,26 +1,98 @@
 import { Spinner, Text } from "@klimadao/lib/components";
+import { Asset, User } from "@klimadao/lib/types/carbonmark";
 import { useWeb3 } from "@klimadao/lib/utils";
-import { NextPage } from "next";
-import { useRouter } from "next/router";
-
+import { t, Trans } from "@lingui/macro";
+import { Activities } from "components/Activities";
 import { Layout } from "components/Layout";
 import { PageHead } from "components/shared/PageHead";
-
+import { Stats } from "components/Stats";
+import { Col, TwoColLayout } from "components/TwoColLayout";
+import { getUserAssetsData } from "lib/actions";
+import { getUser } from "lib/api";
+import {
+  getActiveListings,
+  getAllListings,
+  getAmountLeftToSell,
+  getTotalAmountSold,
+} from "lib/listingsGetter";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { AssetProject } from "./AssetProject";
+import { Balances } from "./Balances";
+
 import * as styles from "./styles";
 
 export const Portfolio: NextPage = () => {
   const router = useRouter();
   const { isConnected, address } = useWeb3();
   const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState<null | User>(null);
+  const [assetsData, setAssetsData] = useState<Asset[] | null>(null);
+  const [isLoadingAssets, setIsLoadingAssets] = useState(false);
+
+  const hasAssets = !!user?.assets?.length;
+  const hasListings = !isLoading && !!user?.listings?.length;
+  const allListings = hasListings && getAllListings(user.listings);
+  const activeListings = hasListings && getActiveListings(user.listings);
+
+  const shouldLoadUser = isConnected && address;
+  const shouldRedirect = !isConnected && !isLoading;
+  const showSpinner = !user || isLoadingAssets;
 
   useEffect(() => {
-    setIsLoading(false);
-  }, []);
+    if (!shouldLoadUser) return;
+
+    const getUserData = async () => {
+      try {
+        const user = await getUser({
+          type: "wallet",
+          user: address,
+        });
+        setUser(user);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    !user && getUserData();
+  }, [shouldLoadUser]);
 
   useEffect(() => {
-    !isLoading && !isConnected && router.push(`/users/login`);
-  }, [isLoading, isConnected]);
+    shouldRedirect && router.push(`/users/login`);
+  }, [shouldRedirect]);
+
+  // load Assets every time user changed
+  useEffect(() => {
+    if (hasAssets && address) {
+      const getAssetsData = async () => {
+        try {
+          setIsLoadingAssets(true);
+          const assetsData = await getUserAssetsData({
+            assets: user.assets,
+            userAddress: address,
+          });
+
+          // TODO: filter assets with balance > 0
+          // this will be unnecessary as soon as bezos switched to mainnet
+
+          const assetsWithBalance = assetsData.filter(
+            (a) => Number(a.balance) > 0
+          );
+
+          setAssetsData(assetsWithBalance);
+        } catch (e) {
+          console.error(e);
+        } finally {
+          setIsLoadingAssets(false);
+        }
+      };
+
+      getAssetsData();
+    }
+  }, [user]);
 
   return (
     <>
@@ -31,20 +103,51 @@ export const Portfolio: NextPage = () => {
       />
 
       <Layout>
-        {!isLoading && isConnected && (
-          <div className={styles.fullWidth}>
-            <Text t="h3">Portfolio</Text>
-            <Text t="caption">For: {address}</Text>
-            <Text t="caption">List ... </Text>
-          </div>
-        )}
-
         {isLoading ||
-          (!isConnected && (
+          (showSpinner && (
             <div className={styles.fullWidth}>
+              <Text>Loading your data</Text>
               <Spinner />
             </div>
           ))}
+
+        {!isLoading && isConnected && !showSpinner && (
+          <TwoColLayout>
+            <Col>
+              {!!assetsData &&
+                assetsData.map((a) => (
+                  <AssetProject key={a.projectId} assetsData={a} />
+                ))}
+              {!assetsData && (
+                <Text>
+                  <Trans>
+                    We couldn't find any C3 tokens in your connected wallet :(
+                  </Trans>
+                </Text>
+              )}
+            </Col>
+
+            <Col>
+              <Balances assetsData={assetsData} />
+              <Stats
+                stats={{
+                  tonnesSold:
+                    (!!allListings && getTotalAmountSold(allListings)) || 0,
+                  tonnesOwned:
+                    (!!activeListings && getAmountLeftToSell(activeListings)) ||
+                    0,
+                  activeListings:
+                    (!!activeListings && activeListings.length) || 0,
+                }}
+                description={t({
+                  id: "user.stats.your_seller_data.description",
+                  message: "Your seller data",
+                })}
+              />
+              <Activities activities={user?.activities || []} />
+            </Col>
+          </TwoColLayout>
+        )}
       </Layout>
     </>
   );
