4439ca5c4937eed272d36f0f51f75ced320e03a5
goats <joeyr84@gmail.com>
2023-02-17 17:36:59 -0500

    build fix

--- carbonmark/components/WebFonts/index.tsx
+++ carbonmark/components/WebFonts/index.tsx
@@ -1,12 +1,9 @@
-import React from "react";
-
 /** <link> elements to load Righteous and Raleway fonts. For Next.js, render this in a <Head> component */
 export const WebFonts = () => {
   return (
     <>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-
       <link
         href="https://fonts.googleapis.com/css2?family=DM+Sans&family=Inter&family=Poppins&display=swap"
         rel="stylesheet"
