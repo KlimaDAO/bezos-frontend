aab1de1c709b474248a097738c9d2fecbf0eec4a
goats <joeyr84@gmail.com>
2023-02-17 12:19:18 -0500

    update font link and add inter back

--- lib/components/WebFonts/index.tsx
+++ lib/components/WebFonts/index.tsx
@@ -6,12 +6,9 @@ export const WebFonts = () => {
     <>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+
       <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
-        rel="stylesheet"
-      />
-      <link
-        href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap"
+        href="https://fonts.googleapis.com/css2?family=DM+Sans&family=Inter&family=Poppins&display=swap"
         rel="stylesheet"
       />
     </>
