72d6bb03038a61842c166d2180d45dbfd52e81d6
Atmosfearful <atmosfearful@protonmail.com>
2023-02-20 11:20:49 -0800

    remove inter and update comments

--- carbonmark/components/WebFonts/index.tsx
+++ carbonmark/components/WebFonts/index.tsx
@@ -5,7 +5,7 @@ export const WebFonts = () => {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
       <link
-        href="https://fonts.googleapis.com/css2?family=DM+Sans&family=Inter&family=Poppins&display=swap"
+        href="https://fonts.googleapis.com/css2?family=DM+Sans&family=Poppins&display=swap"
         rel="stylesheet"
       />
     </>
