0173172a5d8454723b646a0d92dbc39260e5a02a
Atmosfearful <atmosfearful@protonmail.com>
2023-02-20 11:19:53 -0800

    revert theme changes

--- lib/components/WebFonts/index.tsx
+++ lib/components/WebFonts/index.tsx
@@ -6,9 +6,8 @@ export const WebFonts = () => {
     <>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-
       <link
-        href="https://fonts.googleapis.com/css2?family=Inter&family=Poppins&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Poppins:wght@600;700&display=swap"
         rel="stylesheet"
       />
     </>
