2fc71de19a1600f394412e05ad836206616e09cd
ladytrekker <ladytrekker@protonmail.com>
2023-02-23 15:37:29 +0100

    Create PoolPrice and SellerListing component

---
+++ carbonmark/components/pages/Project/BuyOptions/PoolPrice.tsx
@@ -0,0 +1,47 @@
+import { Anchor, ButtonPrimary, PoolIcon } from "@klimadao/lib/components";
+import { t, Trans } from "@lingui/macro";
+import { CarbonmarkButton } from "components/CarbonmarkButton";
+import { Card } from "components/Card";
+import { Text } from "components/Text";
+import { createRetireLink } from "lib/createUrls";
+import { formatToPrice } from "lib/formatNumbers";
+import { PriceFlagged, Project } from "lib/types/carbonmark";
+import { FC } from "react";
+import * as styles from "./styles";
+
+type Props = {
+  price: PriceFlagged;
+  project: Project;
+};
+
+export const PoolPrice: FC<Props> = (props) => {
+  return (
+    <Card>
+      <div className={styles.sellerInfo}>
+        <PoolIcon />
+        <div className={styles.sellerBadge}>
+          <Trans>Carbon Pool</Trans>
+        </div>
+      </div>
+      <Text t="h4">{formatToPrice(props.price.singleUnitPrice)}</Text>
+      <Text t="body1">{props.price.name}</Text>
+
+      <div className={styles.buttons}>
+        <ButtonPrimary
+          className={styles.buyButton}
+          label={t`Buy ${props.price.name}`}
+          href={"https://klimadao.finance"} // go where?
+        />
+
+        <CarbonmarkButton
+          label={t`Retire now`}
+          href={createRetireLink({
+            quantity: props.price.leftToSell,
+            tokenAddress: props.price.tokenAddress,
+          })}
+          renderLink={(linkProps) => <Anchor {...linkProps} />}
+        />
+      </div>
+    </Card>
+  );
+};
