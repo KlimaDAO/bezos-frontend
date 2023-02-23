0e47d24d7b163e45613b7602aebca4e6dc80d86c
ladytrekker <ladytrekker@protonmail.com>
2023-02-23 15:38:28 +0100

    Create listings and price getters and formatters

--- carbonmark/lib/listingsGetter.ts
+++ carbonmark/lib/listingsGetter.ts
@@ -1,6 +1,13 @@
 import { formatUnits } from "@klimadao/lib/utils";
 import { Listing } from "lib/types/carbonmark";
 import { getTokenDecimals } from "lib/networkAware/getTokenDecimals";
+import {
+  Listing,
+  ListingFormatted,
+  Price,
+  PriceFlagged,
+  ProjectBuyOption,
+} from "lib/types/carbonmark";
 
 export const getAmountLeftToSell = (listings: Listing[]) =>
   listings.reduce((acc, curr) => {
@@ -33,6 +40,33 @@ export const getLowestPriceFromListings = (listings: Listing[]) => {
   const decimals = getTokenDecimals("usdc");
   const allPrices = listings.map((l) =>
     Number(formatUnits(l.singleUnitPrice, decimals))
+export const sortPricesAndListingsByBestPrice = (
+  prices: Price[],
+  listings: Listing[]
+): ProjectBuyOption[] => {
+  const flaggedPrices = !!prices?.length && flagPrices(prices);
+  const formattedListings = !!listings?.length && formatListings(listings);
+
+  // Ugly, but otherwise babel throws !
+  const mergedArray = [...(flaggedPrices || []), ...(formattedListings || [])];
+
+  return mergedArray.sort(
+    (a, b) => Number(a.singleUnitPrice) - Number(b.singleUnitPrice)
   );
   return Math.min(...allPrices);
 };
+
+export const formatListings = (listings: Listing[]): ListingFormatted[] =>
+  listings.map((listing) => ({
+    ...listing,
+    singleUnitPrice: formatUnits(
+      listing.singleUnitPrice,
+      getTokenDecimals("usdc")
+    ),
+  }));
+
+export const flagPrices = (prices: Price[]): PriceFlagged[] =>
+  prices.map((price) => ({
+    ...price,
+    isPoolProject: true,
+  }));
