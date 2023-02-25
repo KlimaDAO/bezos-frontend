f7728f3c7cc5a7c1f2742bddbcf334103dce4ad0
emc <0xemc@protonmail.com>
2023-02-25 19:52:33 +1100

    18 - More sort capability

--- carbonmark/components/ProjectFilterModal/constants.tsx
+++ carbonmark/components/ProjectFilterModal/constants.tsx
@@ -2,6 +2,8 @@ import { t } from "@lingui/macro";
 import { CheckboxOption } from "components/CheckboxGroup/CheckboxGroup.types";
 import { Text } from "components/Text";
 import { categoryInfoMap } from "lib/getCategoryInfo";
+import { Project } from "lib/types/carbonmark";
+import { sortBy } from "lib/utils/array.utils";
 import * as styles from "./styles";
 
 export const PROJECT_SORT_OPTIONS = {
@@ -11,6 +13,15 @@ export const PROJECT_SORT_OPTIONS = {
   "vintage-newest": t`Vintage Newest`,
   "vintage-oldest": t`Vintage Oldest`,
 } as const;
+
+/** A mapping of sort option to sort functions */
+export const PROJECT_SORT_FNS = {
+  "price-highest": sortBy<Project>("price", "desc"),
+  "price-lowest": sortBy<Project>("price"),
+  "recently-updated": sortBy<Project>("updatedAt", "desc"),
+  "vintage-newest": sortBy<Project>("vintage", "desc"),
+  "vintage-oldest": sortBy<Project>("vintage"),
+};
 
 /**
  * @todo these will come from the API
--- carbonmark/lib/utils/array.utils.ts
+++ carbonmark/lib/utils/array.utils.ts
@@ -1,3 +1,5 @@
+import { pipe, reverse, sortBy as _sortBy } from "lodash/fp";
+
 /** Return true if the given array is empty*/
 export const empty = <T>(arr: T[]) =>
   arr === null || arr === undefined || arr.length === 0;
@@ -5,3 +7,12 @@ export const empty = <T>(arr: T[]) =>
 /** Return true if the array is empty or it includes the given value */
 export const emptyOrIncludes = <T>(arr: T[], i: T) =>
   empty(arr) || arr?.includes(i);
+
+/** sortBy but reverse */
+const reverseSortBy = <T>(key: keyof T) => pipe(_sortBy<T>(key), reverse);
+
+/** sortBy with an order argument, by default an ascending sort */
+export const sortBy = <T>(
+  key: keyof T,
+  order: "asc" | "desc" = "asc"
+): ((arr: T[]) => T[]) => (order === "asc" ? _sortBy(key) : reverseSortBy(key));
