a0c44240936016db05ae81fdb29e1b82735e2a21
Atmosfearful <atmosfearful@protonmail.com>
2023-02-28 16:15:35 -0800

    do not revalidate on mount

--- carbonmark/hooks/useFetchProjects.ts
+++ carbonmark/hooks/useFetchProjects.ts
@@ -1,3 +1,4 @@
+import { fetcher } from "lib/fetcher";
 import { getProjectsQueryString } from "lib/getProjectsQueryString";
 import { useRouter } from "next/router";
 import useSWR from "swr";
@@ -7,6 +8,8 @@ import { Project } from "../lib/types/carbonmark";
 export const useFetchProjects = () => {
   const router = useRouter();
   const path = `/api/projects` + getProjectsQueryString(router.query);
-  const { data, ...rest } = useSWR<Project[]>(path);
+  const { data, ...rest } = useSWR<Project[]>(path, fetcher, {
+    revalidateOnMount: false,
+  });
   return { projects: data, ...rest };
 };
